94fa8177190179ce96cfae1b5f98508a885f4d22
bifot <bifot@bifot.ru>
2017-11-05 17:42:30 +0300

    v1

---
+++ readme.md
@@ -0,0 +1,75 @@
+[![node-qiwi](https://img.shields.io/npm/v/node-qiwi.svg?style=flat-square)](https://www.npmjs.com/package/node-qiwi/)
+[![node-qiwi](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square)](http://standardjs.com/)
+
+# node-qiwi
+
+API for Qiwi Wallet.
+
+## Install
+
+```sh
+$ npm i node-qiwi
+```
+
+## Tests
+
+```sh
+$ npm test
+```
+
+## Docs
+
+* [constructor(key)](#constructorkey)
+* [.getProfile(settings)](#getprofilesettings) ⇒ `[Promise]`
+* [.getBalance()](#getbalance) ⇒ `[Promise]`
+* [.getHistory(contractId, settings)](#gethistorycontractid-settings) ⇒ `[Promise]`
+* [.sendPayment(amount, account, comment)](#sendpaymentamount-account-comment) ⇒ `[Promise]`
+
+### constructor(key)
+
+| Parameter  | Type      | Requried  | Description  | 
+| -----------|:---------:|:---------:|-------------:|
+| key        | string    | yes       | Access token for call API methods |
+
+Create wallet.
+
+### .getProfile(settings)
+
+| Parameter  | Type      | Requried  | Description  | 
+| -----------|:---------:|:---------:|-------------:|
+| settings   | object    | no        | Extra settings ([see](https://developer.qiwi.com/ru/qiwi-wallet-personal/#profile)) |
+
+Get profile.
+
+### .getBalance()
+
+Get balance of all accounts.
+
+### .getHistory(contractId, settings)
+
+| Parameter  | Type      | Requried  | Description  | 
+| -----------|:---------:|:---------:|-------------:|
+| contractId | string    | yes       | Contract Id of wallet |
+| settings   | object    | no        | Extra settings ([see](https://developer.qiwi.com/ru/qiwi-wallet-personal/#payments_history)) |
+
+```js
+const { contractInfo: { contractId } } = await wallet.getProfile()
+const history = await wallet.getHistory(contractId)
+```
+
+Get history.
+
+### .sendPayment(amount, account, comment)
+
+| Parameter  | Type          | Requried  | Description  | 
+| -----------|:-------------:|:---------:|-------------:|
+| amount     | number/string | yes       | Transaction amount     |
+| account    | number/string | yes       | Recipient's account    |
+| comment    | string        | no        | Comment to transaction |
+
+Send payment.
+
+## License
+
+MIT.
+
---
+++ src/Qiwi.js
@@ -0,0 +1,118 @@
+const rp = require('request-promise')
+
+module.exports = class Qiwi {
+  constructor (key) {
+    this.key = key
+    this.baseUrl = 'https://edge.qiwi.com/'
+  }
+
+  handlerError (err) {
+    const { message: error } = err.error
+    return { error }
+  }
+
+  async getProfile (settings) {
+    const { baseUrl, key } = this
+
+    try {
+      const profile = await rp({
+        baseUrl,
+        url: '/person-profile/v1/profile/current',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${key}`
+        },
+        qs: settings,
+        json: true
+      })
+
+      return profile
+    } catch (err) {
+      return this.handlerError(err)
+    }
+  }
+
+  async getBalance () {
+    const { baseUrl, key } = this
+
+    try {
+      const { accounts } = await rp({
+        baseUrl,
+        url: '/funding-sources/v1/accounts/current',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${key}`
+        },
+        json: true
+      })
+
+      return accounts
+    } catch (err) {
+      return this.handlerError(err)
+    }
+  }
+
+  async getHistory (contractId, settings) {
+    const { baseUrl, key } = this
+
+    try {
+      const { data } = await rp({
+        baseUrl,
+        url: `/payment-history/v1/persons/${contractId}/payments`,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${key}`
+        },
+        qs: {
+          rows: 50,
+          ...settings
+        },
+        json: true
+      })
+
+      return data
+    } catch (err) {
+      return this.handlerError(err)
+    }
+  }
+
+  async sendPayment (amount, account, comment) {
+    const { baseUrl, key } = this
+
+    try {
+      const payment = await rp({
+        baseUrl,
+        url: '/sinap/api/v2/terms/99/payments',
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${key}`
+        },
+        body: {
+          id: String(new Date().getTime() * 1000),
+          sum: {
+            amount,
+            currency: '643'
+          },
+          paymentMethod: {
+            type: 'Account',
+            accountId: '643'
+          },
+          fields: {
+            account
+          },
+          comment
+        },
+        json: true
+      })
+
+      return payment
+    } catch (err) {
+      return this.handlerError(err)
+    }
+  }
+}
