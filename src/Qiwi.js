67c8af1de3af7017caef50a397ae70c5b1a9f18f
bifot <bifot@bifot.ru>
2017-11-12 14:55:40 +0300

    new methods & tests

--- src/Qiwi.js
+++ src/Qiwi.js
@@ -7,8 +7,127 @@ module.exports = class Qiwi {
   }
 
   handlerError (err) {
-    const { message: error } = err.error
+    const { code, message } = err
+
+    if (code === 500 && message !== undefined) {
+      return { error: message }
+    }
+
+    const { error } = err
     return { error }
+  }
+
+  async getIdentification (contractId, body) {
+    const { baseUrl, key } = this
+
+    try {
+      const identification = await rp({
+        baseUrl,
+        url: `/identification/v1/persons/${contractId}/identification`,
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${key}`
+        },
+        body,
+        json: true
+      })
+
+      return identification
+    } catch (err) {
+      return this.handlerError(err)
+    }
+  }
+
+  async getHistory (contractId, settings) {
+    const { baseUrl, key } = this
+
+    try {
+      const { data } = await rp({
+        baseUrl,
+        url: `/payment-history/v1/persons/${contractId}/payments`,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${key}`
+        },
+        qs: {
+          rows: 50,
+          ...settings
+        },
+        json: true
+      })
+
+      return data
+    } catch (err) {
+      return this.handlerError(err)
+    }
+  }
+
+  async getTransactionsStats (contractId, settings = {}) {
+    const { baseUrl, key } = this
+    const { startDate, endDate } = settings
+
+    try {
+      if (!startDate) {
+        throw {
+          code: 500,
+          message: 'startDate is required param'
+        }
+      } else if (!endDate) {
+        throw {
+          code: 500,
+          message: 'endDate is required param'
+        }
+      }
+
+      const stats = await rp({
+        baseUrl,
+        url: `/payment-history/v1/persons/${contractId}/payments/total`,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${key}`
+        },
+        qs: settings,
+        json: true
+      })
+
+      return stats
+    } catch (err) {
+      return this.handlerError(err)
+    }
+  }
+
+  async getTransaction (transactionId, settings = {}) {
+    const { baseUrl, key } = this
+    const { type } = settings
+
+    try {
+      if (!type) {
+        throw {
+          code: 500,
+          message: 'type is required param'
+        }
+      }
+
+      const transaction = await rp({
+        baseUrl,
+        url: `/payment-history/v1/transactions/${transactionId}`,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${key}`
+        },
+        qs: settings,
+        json: true
+      })
+
+      return transaction
+    } catch (err) {
+      return this.handlerError(err)
+    }
   }
 
   async getProfile (settings) {
@@ -49,31 +168,6 @@ module.exports = class Qiwi {
       })
 
       return accounts
-    } catch (err) {
-      return this.handlerError(err)
-    }
-  }
-
-  async getHistory (contractId, settings) {
-    const { baseUrl, key } = this
-
-    try {
-      const { data } = await rp({
-        baseUrl,
-        url: `/payment-history/v1/persons/${contractId}/payments`,
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${key}`
-        },
-        qs: {
-          rows: 50,
-          ...settings
-        },
-        json: true
-      })
-
-      return data
     } catch (err) {
       return this.handlerError(err)
     }
---
+++ test/test.js
@@ -0,0 +1,204 @@
+const { expect } = require('chai')
+const Qiwi = require('../src/Qiwi')
+
+const wallet = new Qiwi(process.env.TOKEN)
+
+describe('QIWI', function () {
+  this.timeout(5000)
+
+  it('GET identification', async () => {
+    const { contractInfo: { contractId } } = await wallet.getProfile()
+    const identification = await wallet.getIdentification(contractId, {
+      firstName: 'Иван',
+      lastName: 'Иванов',
+      middleName: 'Иванович',
+      birthDate: '1998-02-11',
+      passport: 4400111222
+    })
+
+    expect(identification)
+      .to.be.a('object')
+      .to.have.all.keys([
+        'id',
+        'firstName',
+        'middleName',
+        'lastName',
+        'birthDate',
+        'passport',
+        'inn',
+        'snils',
+        'oms',
+        'type'
+      ])
+  })
+
+  it('GET history', async () => {
+    const { contractInfo: { contractId } } = await wallet.getProfile()
+    const history = await wallet.getHistory(contractId)
+
+    expect(history).to.be.a('array')
+
+    history.forEach((item) => {
+      expect(item)
+        .to.be.a('object')
+        .to.have.keys([
+          'txnId',
+          'personId',
+          'date',
+          'errorCode',
+          'error',
+          'status',
+          'type',
+          'statusText',
+          'trmTxnId',
+          'account',
+          'sum',
+          'commission',
+          'total',
+          'provider',
+          'source',
+          'comment',
+          'currencyRate',
+          'extras',
+          'chequeReady',
+          'bankDocumentAvailable',
+          'bankDocumentReady',
+          'repeatPaymentEnabled',
+          'favoritePaymentEnabled',
+          'regularPaymentEnabled'
+        ])
+    })
+  })
+
+  it('GET transactions stats', async () => {
+    const startDate = new Date()
+    const endDate = new Date()
+
+    startDate.setDate(1)
+
+    const { contractInfo: { contractId } } = await wallet.getProfile()
+    const stats = await wallet.getTransactionsStats(contractId, {
+      startDate,
+      endDate
+    })
+
+    expect(stats)
+      .to.be.a('object')
+      .to.have.all.keys([ 'incomingTotal', 'outgoingTotal' ])
+  })
+
+  it('GET transaction', async () => {
+    const { contractInfo: { contractId } } = await wallet.getProfile()
+    const [ transaction ] = await wallet.getHistory(contractId)
+    const { txnId, type } = transaction
+    const transactionData = await wallet.getTransaction(txnId, { type })
+
+    expect(transaction)
+      .to.be.a('object')
+      .to.have.all.keys([
+        'txnId',
+        'personId',
+        'date',
+        'errorCode',
+        'error',
+        'status',
+        'type',
+        'statusText',
+        'trmTxnId',
+        'account',
+        'sum',
+        'commission',
+        'total',
+        'provider',
+        'source',
+        'comment',
+        'currencyRate',
+        'extras',
+        'chequeReady',
+        'bankDocumentAvailable',
+        'bankDocumentReady',
+        'repeatPaymentEnabled',
+        'favoritePaymentEnabled',
+        'regularPaymentEnabled'
+      ])
+  })
+
+  it('GET profile', async () => {
+    const profile = await wallet.getProfile()
+    const { authInfo, contractInfo, userInfo } = profile
+
+    expect(profile)
+      .to.be.a('object')
+      .to.have.keys([
+        'authInfo',
+        'contractInfo',
+        'userInfo'
+      ])
+    expect(authInfo)
+      .to.be.a('object')
+      .to.have.keys([
+        'personId',
+        'pinInfo',
+        'passInfo',
+        'registrationDate',
+        'boundEmail',
+        'ip',
+        'lastLoginDate',
+        'mobilePinInfo'
+      ])
+    expect(contractInfo)
+      .to.be.a('object')
+      .to.have.keys([
+        'contractId',
+        'creationDate',
+        'features',
+        'identificationInfo',
+        'blocked'
+      ])
+    expect(userInfo)
+      .to.be.a('object')
+      .to.have.keys([
+        'defaultPayAccountAlias',
+        'defaultPayCurrency',
+        'defaultPaySource',
+        'firstTxnId',
+        'language',
+        'operator',
+        'phoneHash'
+      ])
+  })
+
+  it('GET balance', async () => {
+    const balance = await wallet.getBalance()
+
+    expect(balance).to.be.a('array')
+
+    balance.forEach((item) => {
+      expect(item)
+        .to.be.a('object')
+        .to.have.keys([
+          'alias',
+          'fsAlias',
+          'title',
+          'type',
+          'hasBalance',
+          'balance',
+          'currency'
+        ])
+    })
+  })
+
+  // it('SEND payment', async () => {
+  //   const payment = await wallet.sendPayment(1, '37253676697', 'This is comment')
+  //
+  //   expect(payment).to.be.a('object').to.have.keys([
+  //     'id',
+  //     'terms',
+  //     'fields',
+  //     'sum',
+  //     'transaction',
+  //     'comment',
+  //     'source'
+  //   ])
+  // })
+})
